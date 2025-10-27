4b7919ac9af355cf6a57e17a21d19690d9205347
konarjg <konarskikrzysztof1@gmail.com>
2025-10-27 20:52:59 +0100

    Added proto contract

---
+++ ChatServer/ChatServer/Services/AuthServiceImpl.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatServer.Services;
+
+using Chat;
+using Grpc.Core;
+
+public class AuthServiceImpl : AuthService.AuthServiceBase {
+  public override Task<AuthResponse> Login(LoginRequest request,
+    ServerCallContext context) {
+    throw new NotImplementedException();
+  }
+}
